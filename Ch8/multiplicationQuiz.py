b6a2dc0d12b95446f4f5880441a327eb9bda342b
Roberto Cases <roberto.cases@gmail.com>
2020-03-06 12:38:46 -0800

    fixed except syntax error

--- Ch8/multiplicationQuiz.py
+++ Ch8/multiplicationQuiz.py
@@ -14,13 +14,13 @@ try:
     # Right answers are handled by allowRegexes.
     # Wrong answers are handled by blockRegexes, with a custom message.
     pyip.inputStr(prompt, allowRegexes=['^%s$' % (num1 * num2)], blockRegexes=[('.*', 'Incorrect!')],timeout=8, limit=3)
-    except pyip.TimeoutException:
-        print('Out of time!')
-    except pyip.RetryLimitException:
-        print('Out of tries!')
-    else:
-        # This block runs if no exceptions were raised in the try block.
-        print('Correct!')
-        correctAnswers += 1
-    time.sleep(1) #brief pause to let user see the result.
+except pyip.TimeoutException:
+    print('Out of time!')
+except pyip.RetryLimitException:
+    print('Out of tries!')
+else:
+    # This block runs if no exceptions were raised in the try block.
+    print('Correct!')
+    correctAnswers += 1
+time.sleep(1) #brief pause to let user see the result.
 print('Score: %s / %s' % (correctAnswers, numberOfQuestions))
