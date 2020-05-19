ad47726fa39e90c159a7b18d72e867ff977f0ecc
Roberto Cases <roberto.cases@gmail.com>
2020-05-19 15:59:09 -0700

    Step 1 fix

--- Ch10/renameDates.py
+++ Ch10/renameDates.py
@@ -10,7 +10,7 @@ datePattern = re.compile(r"""^(.*?) # all text before the date
        ((0|1|2|3)?\d)-                 # one or two digits for the day
        ((19|20)\d\d)                   # four digits for the year
        (.*?)$                          # all text after the date
-       """, re.VERBOSE➌)
+       """, re.VERBOSE)
 
 # TODO: Loop over the files in the working directory.
 
