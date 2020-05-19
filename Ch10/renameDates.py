dd340eeea80986f7fb46f4db153c83d5e2df8dbb
Roberto Cases <roberto.cases@gmail.com>
2020-05-19 16:10:02 -0700

    Step 3

--- Ch10/renameDates.py
+++ Ch10/renameDates.py
@@ -13,10 +13,21 @@ datePattern = re.compile(r"""^(.*?) # all text before the date
        """, re.VERBOSE)
 
 # TODO: Loop over the files in the working directory.
+for amerFilename in os.listdir('.'):
+
+    mo = datePattern.search(amerFilename)
 
 # TODO: Skip files without a date.
+    if mo == None:
+
+        continue
 
 # TODO: Get the different parts of the filename.
+    beforePart =  mo.group(1)
+    monthPart = mo.group(2)
+    dayPart = mo.group(4)
+    yearPart = mo.group(6)
+    afterPart = mo.group(8)
 
 # TODO: Form the European-style filename.
 
