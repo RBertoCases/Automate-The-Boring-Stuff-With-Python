9a2fffd25d57d353e5578ebf0f2b7216d2e70214
RBertoCases <roberto.cases@gmail.com>
2020-06-24 11:45:44 -0700

    Step 2

--- searchPyPI.py
+++ searchPyPI.py
@@ -7,6 +7,7 @@ res = requests.get('https://google.com/search?q=' 'https://pypi.org/search/?q='
 + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
-# TODO: Retrieve top search result links.
-
-# TODO: Open a browser tab for each result.
+# Retrieve top search result links.
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+# Open a browser tab for each result.
+linkElems = soup.select('.package-snippet')
