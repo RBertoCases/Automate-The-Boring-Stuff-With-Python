ded0a774caf5c83d5957980039d23ee06f2f9209
RBertoCases <roberto.cases@gmail.com>
2020-06-24 12:00:25 -0700

    modified

--- Ch12/searchPyPI.py
+++ Ch12/searchPyPI.py
@@ -1,18 +1,16 @@
 #! python3
-# searchpypi.py  - Opens several search results.
+# Open several Google search results.
 
 import requests, sys, webbrowser, bs4
-print('Searching...')    # display text while downloading the search result page
-res = requests.get('https://google.com/search?q=' 'https://pypi.org/search/?q='
-+ ' '.join(sys.argv[1:]))
+
+print('Searching...') # display text while downloading the search result page
+res = requests.get('http://pypi.org/search/?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
-# Retrieve top search result links.
-soup = bs4.BeautifulSoup(res.text, 'html.parser')
-# Open a browser tab for each result.
+soup = bs4.BeautifulSoup(res.text, features='html.parser')
 linkElems = soup.select('.package-snippet')
 numOpen = min(5, len(linkElems))
 for i in range(numOpen):
-    urlToOpen = 'https://pypi.org' + linkElems[i].get('href')
+    urlToOpen = 'http://pypi.org' + linkElems[i].get('href')
     print('Opening', urlToOpen)
     webbrowser.open(urlToOpen)
