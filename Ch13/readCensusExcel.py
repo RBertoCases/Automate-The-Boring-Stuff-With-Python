b0fdf0dd1f23aabca91f1c2e0a0218eeb0f7c8be
Roberto Cases <roberto.cases@gmail.com>
2020-08-27 09:01:03 -0700

    Step 3

--- Ch13/readCensusExcel.py
+++ Ch13/readCensusExcel.py
@@ -25,4 +25,9 @@ for row in range(2, sheet.max_row + 1):
     # Increase the county pop by the pop in this census tract.
     countyData[state][county]['pop'] += int(pop)
 
-# TODO: Open a new text file and write the contents of countyData to it.
+# Open a new text file and write the contents of countyData to it.
+print('Writing results...')
+resultFile = open('census2010.py', 'w')
+resultFile.write('allData = ' + pprint.pformat(countyData))
+resultFile.close()
+print('Done.')
